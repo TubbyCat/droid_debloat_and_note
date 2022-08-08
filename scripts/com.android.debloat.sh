6913272a8032cbec998bc984f6391080ca2663c7
TubbyCat <tubbycat.dev.pub@protonmail.com>
2022-08-08 09:00:22 -0400

    Update com.android.debloat.sh

--- scripts/com.android.debloat.sh
+++ scripts/com.android.debloat.sh
@@ -1,1 +1,21 @@
+#Copyright © 2022-2023 TubbyCat
+#MIT and CREDIT files for elaborated copyright.
 
+#!/bin/env bash 
+
+com_android_list=(
+com.android.chrome
+com.android.egg
+com.android.printspooler
+com.android.bips
+com.android.bluetoothmidiservice
+com.android.traceur
+com.android.aftersaleservice
+com.android.musicfx
+)
+
+echo "Debloating com.android apk variants."
+
+for x in ${com_android_list[@]}; do 
+adb shell pm uninstall --user 0 $x && echo $x >> com_android.debloated.$(date +"%m_%d_%Y").success
+done
