aff5561e881f2e1e422b1437b4491b1be49c619b
TubbyCat <tubbycat.dev.pub@protonmail.com>
2022-08-08 08:47:21 -0400

    Create template.sh

---
+++ scripts/template.sh
@@ -0,0 +1,14 @@
+#Copyright © 2022-2023 TubbyCat
+#MIT and CREDIT files for elaborated copyright.
+#!/bin/env bash
+
+
+~apktype_placeholder~_list=( 
+
+)
+
+echo "Debloating APKTYPEPLACEHOLDER"
+
+for x in ${~REMOVAL_LIST_PLACEHOLDER~[@]}; do 
+adb shell pm uninstall --user 0 $x && echo $x >> APKTYPEPLACEHOLDER.debloated.$(date +"%m_%d_%Y").success
+done
