66b6ae7e9ef99f1f414f1a2f24c98e48335de783
TubbyCat <tubbycat.dev.pub@protonmail.com>
2022-08-08 04:21:54 -0400

    Create zte_device_debloat.testdevice.sh

---
+++ experiments/zte/zte_device_debloat.testdevice.sh
@@ -0,0 +1,27 @@
+#Copyright © 2022-2023 TubbyCat
+#MIT and CREDIT files for elaborated copyright.
+
+#!/bin/env bash 
+
+function caution(){
+echo "Precautionary! Pre-download desired side-loadable apps e.g. alternative file manager, gui in case things go awry. So you may then adb shell install $package."
+echo " ADB debloat snippet for zte only. TESTING." 
+sleep 1
+}
+
+rmv_list=(
+cn.zte.recorder
+com.zte.weather
+com.zte.zdm.omacp
+com.zte.videoplayer
+com.zte.assistant
+com.zte.heartyservice.strategy
+com.zte.burntest.camera
+com.zte.contacts.sub
+com.zte.privacypolicy
+)
+
+caution 
+for i in ${rmv_list[@]}; do 
+adb shell pm uninstall --user 0 $i" 
+done
